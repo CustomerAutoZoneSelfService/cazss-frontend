caa951897480063e75c7e13d2679c7560bca3626
Silvia Amaresly Domínguez García <119064337+AMARESLYY@users.noreply.github.com>
2025-04-10 22:43:28 -0600

    feat: add List component with horizontal and vertical support

---
+++ src/_components/list.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let className: string = '';
+    export let listStyle:'list-decimal' | 'list-disc' = 'list-decimal';
+    export let direction: 'vertical' | 'horizontal' = 'vertical';
+    export let type: 'ul' | 'ol' = 'ul';
+  
+    const isHorizontal = direction === 'horizontal';
+  
+    const baseStyles = isHorizontal
+      ? 'flex gap-8 p-2'
+      : 'flex flex-col gap-6 p-2';
+  
+  </script>
+  
+  {#if type === 'ul'}
+    <ul class={`${baseStyles} ${listStyle} ${className}`}>
+      <slot />
+    </ul>
+  {:else}
+    <ol class={`${baseStyles} ${listStyle} ${className}`}>
+      <slot />
+    </ol>
+  {/if}
