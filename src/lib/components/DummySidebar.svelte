5ff9dca9694d4e850b8d9d0efa5fd3a683ebf926
Gustavo B. <117482958+DevEquinox@users.noreply.github.com>
2025-04-11 17:38:24 -0600

    feature: added user account dropdown menu and an animated loading svg (FE-75)

---
+++ src/lib/components/DummySidebar.svelte
@@ -0,0 +1,48 @@
+<script>
+	import UserAccountDropdown from '$lib/components/UserAccountDropdown.svelte';
+
+	let username = "John Doe";
+	let email = "some@domain.com";
+	let isUserDisplayOn = $state(false);
+
+	function handleMenuOpen() {
+		isUserDisplayOn = true
+		console.log('open')
+		document.body.addEventListener('click', handleMenuClose)
+	}
+
+	function handleMenuClose() {
+		isUserDisplayOn = false
+		console.log('close')
+		document.body.removeEventListener('click', handleMenuClose)
+	}
+</script>
+<div class="dummy-flex">
+	<div>
+		qwerty
+	</div>
+	<div>
+		asdf
+	</div>
+	<div class="user-menu-wrapper">
+		<button on:click|stopPropagation={handleMenuOpen}>{username} (Click me)</button>
+		<UserAccountDropdown username = {username} email = {email} isUserDisplayOn = {isUserDisplayOn}/>
+	</div>
+</div>
+
+<style>
+    .dummy-flex {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    button {
+        text-align: left;
+    }
+
+    .user-menu-wrapper {
+        position: relative;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="text-primary" >Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import DummySidebar from '$lib/components/DummySidebar.svelte';
+</script>
+
+<DummySidebar/>
