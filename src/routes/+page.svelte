936ca0d4e771e5855f9427f41cee52e0e7a2bc75
LouArcTec <a01562967@tec.mx>
2025-04-11 00:05:11 -0600

    chore: rebase branch to new main (FE-72)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 <h1 class="text-primary" >Welcome to SvelteKit</h1>
+=======
+<h1>Welcome to SvelteKit</h1>
+>>>>>>> 506b492 (feat: initial commit (TD-000))
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
