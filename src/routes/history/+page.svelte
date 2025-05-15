2fff011a9086fbbaca629e1c1d2b1e4059eb1170
Edgar Olg <edgar.fer.a01@gmail.com>
2025-05-14 21:22:01 -0600

    chore: creation of the history page (FE-1)

---
+++ src/routes/history/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<div class="align-items justify-center">
+	<main class="flex-1 bg-white p-8">
+		<div class="mb-6 flex items-center justify-between">
+			<div>
+				<h1 class="text-2xl font-bold">History</h1>
+			</div>
+			<div>
+				<p>container for all the cards</p>
+			</div>
+		</div>
+	</main>
+</div>
