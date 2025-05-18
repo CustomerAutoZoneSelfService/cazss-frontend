cf24752b10880656a327b835aac9d8a47dab1d64
DanaGtzDev <167911843+DanaGtzDev@users.noreply.github.com>
2025-05-17 21:06:50 -0600

    feat: Execute service conditional rendering if response is a pdf encoded in base 64 (FE-11) (#12)
    Co-authored-by: DGutierrezLeal <a01568890@tec.mx>

--- src/routes/endpoint/[id]/+page.svelte
+++ src/routes/endpoint/[id]/+page.svelte
@@ -89,32 +89,6 @@
 		response: []
 	});
 
-	// Mock Response to test UI if the backend is unavailable / the database is empty
-	/*
-	let mockExecutionResponse:ServiceResponse = {
-		status: {
-    code: 200,
-    description: "Success",
-  	},
-		response: [
-			{
-				"firstName": ["John"],
-				"lastName": ["Doe"],
-				"age": ["30"],
-			},
-			{
-				"productName": ["Laptop"],
-				"price": ["1200"],
-				"features": ["Intel Core i7", "16GB RAM", "512GB SSD"],
-			},
-			{
-				"city": ["Chihuahua"],
-				"country": ["Mexico"],
-			},
-		],
-	};
-	*/
-
 	// Function to initialize the RequestService object with the variables from the endpoint and possible default values
 	const initializeRequestService = (variables: RequestVariableString[]) => {
 		const newHeaders: KeyValue[] = [];
@@ -185,42 +159,6 @@
 		}
 	};
 
-	// This is a mock function to simulate the fetchEndpoint function if no backend available
-	/*
-	const fetchEndpoint = async ( id:number ) => {
-		endpoint = {
-			id: id,
-			name: "Get PNA by SKU",
-			description: "This endpoint retrieves the PNA by SKU.",
-			active: true,
-			method: 'GET',
-			url: 'https:blabla',
-			responses: [{
-				status: 20,
-				description: 'OK',
-			}
-			],
-			filters: [],
-			variables: [
-				{requestVariableId: 1,
-				type: 'HEADER',
-				keyName: 'CLIENT ID',
-				defaultValue: '',
-				customizable: true,
-				description: 'The id of the client'},
-				{requestVariableId: 2,
-					type: 'BODY',
-					keyName: 'SKU',
-					defaultValue: '00000',
-					customizable: true,
-					description: 'The SKU of the product'}
-			],
-			requestBody: 'Template'
-		};
-
-		initializeRequestService(endpoint.variables);
-	}
-	*/
 	const handleSend = async () => {
 		try {
 			await executeEndpoint();
@@ -310,10 +248,17 @@
 			</List>
 
 			<HeadingFormat as="h4">Raw body</HeadingFormat>
-			<TextArea
-				disabled={true}
-				text={ExecutionResponse.response ? JSON.stringify(ExecutionResponse.response) : ''}
-			/>
+			{#if ExecutionResponse.response.some((variable) => 'content' in variable)}
+				<!--
+				<PdfViewer data={atob(ExecutionResponse.response.find(v => 'content' in v)?.content)} />
+				-->
+				<p>Here goes the pdf visualizer and the button</p>
+			{:else}
+				<TextArea
+					disabled={true}
+					text={ExecutionResponse.response ? JSON.stringify(ExecutionResponse.response) : ''}
+				/>
+			{/if}
 		{/if}
 	</div>
 </main>
