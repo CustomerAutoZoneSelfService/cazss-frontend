3966b34be7799fba7ab80a6285cb525bac6ae9d4
DanaGtzDev <167911843+DanaGtzDev@users.noreply.github.com>
2025-05-17 21:06:50 -0600

    feat: Execute service conditional rendering if response is a pdf encoded in base 64 (FE-11) (#12)
    Co-authored-by: DGutierrezLeal <a01568890@tec.mx>

---
+++ e2e/mock/pdfview.test.ts
@@ -0,0 +1,25 @@
+import { test } from '@playwright/test';
+import { initE2EMocked } from '../initE2EMocked';
+
+const pages = ['endpoint/5'];
+
+for (const path of pages) {
+	test.beforeEach(async ({ page }, testInfo) => {
+		await initE2EMocked(page, testInfo); // Call the initialization function
+		await page.goto(`${path}`);
+	});
+
+	/**
+     * test('should have an h1 with text "Pdf demo"', async ({ page }) => {
+		const h1 = page.locator('h1', { hasText: 'Pdf demo' });
+		await expect(h1).toBeVisible();
+	});
+
+	test('should show PDF visualizer message after clicking Send', async ({ page }) => {
+		await page.getByRole('button', { name: 'Send' }).click();
+
+		const paragraph = page.locator('p', { hasText: 'Here goes the pdf visualizer and the button' });
+		await expect(paragraph).toBeVisible();
+	});
+     */
+}
--- src/lib/mockApiWrapper.ts
+++ src/lib/mockApiWrapper.ts
@@ -9,70 +9,109 @@ export function replaceWithMock(mockApi: ApiWrapper): void {
 			{ endpointId: 1, name: 'getById', description: 'Example description' },
 			{ endpointId: 2, name: 'getByName', description: 'Example description 2' },
 			{ endpointId: 3, name: 'clearCache', description: 'Example description 3' },
-			{ endpointId: 4, name: 'resetSomething', description: 'Example description 4' }
+			{ endpointId: 4, name: 'resetSomething', description: 'Example description 4' },
+			{ endpointId: 5, name: 'pdfMockData', description: 'Example pdf endpoint' }
 		];
 	};
 
-	mockApi.getServiceById = async function (): Promise<DetailedService> {
-		return {
-			id: 1,
-			name: 'Get PNA by SKU',
-			description: 'This endpoint retrieves the PNA by SKU.',
-			active: true,
-			method: 'GET',
-			url: 'https:blabla',
-			responses: [
-				{
-					status: 20,
-					description: 'OK'
-				}
-			],
-			filters: [],
-			variables: [
-				{
-					requestVariableId: 1,
-					type: 'HEADER',
-					keyName: 'CLIENT ID',
-					defaultValue: '',
-					customizable: true,
-					description: 'The id of the client'
-				},
-				{
-					requestVariableId: 2,
-					type: 'BODY',
-					keyName: 'SKU',
-					defaultValue: '00000',
-					customizable: true,
-					description: 'The SKU of the product'
-				}
-			],
-			requestBody: 'Template'
-		};
+	mockApi.getServiceById = async function (id: number): Promise<DetailedService> {
+		if (id.toString() !== '5') {
+			return {
+				id: 1,
+				name: 'Get PNA by SKU',
+				description: 'This endpoint retrieves the PNA by SKU.',
+				active: true,
+				method: 'GET',
+				url: 'https:blabla',
+				responses: [
+					{
+						status: 20,
+						description: 'OK'
+					}
+				],
+				filters: [],
+				variables: [
+					{
+						requestVariableId: 1,
+						type: 'HEADER',
+						keyName: 'CLIENT ID',
+						defaultValue: '',
+						customizable: true,
+						description: 'The id of the client'
+					},
+					{
+						requestVariableId: 2,
+						type: 'BODY',
+						keyName: 'SKU',
+						defaultValue: '00000',
+						customizable: true,
+						description: 'The SKU of the product'
+					}
+				],
+				requestBody: 'Template'
+			};
+		} else {
+			return {
+				id: 5,
+				name: 'Pdf Demo',
+				description: 'This endpoint tests the pdf functionality',
+				active: true,
+				method: 'GET',
+				url: 'https:blabla',
+				responses: [
+					{
+						status: 20,
+						description: 'OK'
+					}
+				],
+				filters: [],
+				variables: [],
+				requestBody: 'Template'
+			};
+		}
 	};
 
-	mockApi.executeService = async function (): Promise<ServiceResponse> {
-		return {
-			status: {
-				code: 200,
-				description: 'Success'
-			},
-			response: [
-				{
-					firstName: ['John'],
-					lastName: ['Doe'],
-					age: ['30']
+	mockApi.executeService = async function (id: number): Promise<ServiceResponse> {
+		if (id.toString() !== '5') {
+			return {
+				status: {
+					code: 200,
+					description: 'Success'
 				},
-				{
-					productName: ['Laptop'],
-					price: ['1200'],
-					features: ['Intel Core i7', '16GB RAM', '512GB SSD']
+				response: [
+					{
+						firstName: ['John'],
+						lastName: ['Doe'],
+						age: ['30']
+					},
+					{
+						productName: ['Laptop'],
+						price: ['1200'],
+						features: ['Intel Core i7', '16GB RAM', '512GB SSD']
+					},
+					{
+						city: ['Chihuahua'],
+						country: ['Mexico']
+					}
+				]
+			};
+		} else {
+			let textContent = '';
+			const res = await fetch('/test_pdf.txt');
+			textContent = await res.text();
+
+			return {
+				status: {
+					code: 200,
+					description: 'Success'
 				},
-				{
-					city: ['Chihuahua'],
-					country: ['Mexico']
-				}
-			]
-		};
+				response: [
+					{
+						content: [textContent]
+					}
+				]
+			};
+		}
 	};
 
 	mockApi.getAllHistory = async function (): Promise<HistoryService[]> {
