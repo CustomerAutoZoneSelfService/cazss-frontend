267d4bc301e3ecbd780a11b4458d46df55cc02dd
Gustavo Barraza <117482958+DevEquinox@users.noreply.github.com>
2025-05-18 00:57:08 -0600

    refactor: bring build time fix, Sidebar bugfix, test fix and commit-msg fix to team-3-stable (SCRUM-82) (#17)

--- e2e/initE2EMocked.ts
+++ e2e/initE2EMocked.ts
@@ -1,15 +1,10 @@
-// e2e/initE2EMocked.ts
 import type { Page, TestInfo } from '@playwright/test';
 
 export async function initE2EMocked(page: Page, testInfo: TestInfo): Promise<void> {
 	const isMockedFromEnv = testInfo.project?.use?.launchOptions?.env?.E2E_MOCKED === 'true';
 
-	await page.evaluate((isMocked) => {
-		window.E2E_MOCKED = isMocked;
-		console.log(isMocked);
-		console.log(window.E2E_MOCKED);
-		// Helps create a global variable so AppContainer can access it,
-		// which results in it knowing if it's going to use the mock version or not.
+	await page.addInitScript(mock => {
+		window.E2E_MOCKED = mock;
 	}, isMockedFromEnv);
 }
 
