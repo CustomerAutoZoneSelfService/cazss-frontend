88e8a54cea724f6f66be468a743f9abc85053831
Edgar A <115832121+edgarolg@users.noreply.github.com>
2025-05-18 01:01:02 -0600

    chore: fix overlaping of text (SCRUM-192) (#14)
    Co-authored-by: LouArcTec <a01562967@tec.mx>

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -20,7 +20,7 @@
 	 */
 	function computeClasses() {
 		let baseClasses =
-			'inline-flex items-center justify-center font-bold rounded-md ' +
+			'inline-flex items-center justify-center font-bold rounded-md cursor-pointer ' +
 			'focus:outline-none transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed';
 
 		let variantClasses = '';
