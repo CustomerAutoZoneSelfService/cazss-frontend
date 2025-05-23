f04ee0370f11d8144f4fedf47df8001f18313061
DanaGtzDev <167911843+DanaGtzDev@users.noreply.github.com>
2025-05-22 20:08:31 -0600

    Feat: pagination on send request (FE-20) (#21)
    Co-authored-by: DGutierrezLeal <a01568890@tec.mx>

---
+++ src/lib/components/DisplayResultSectionInvokeService.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import HeadingFormat from '$lib/components/HeadingFormat.svelte';
+	import Tooltip from '$lib/components/Tooltip.svelte';
+	import List from '$lib/components/List.svelte';
+	import TextArea from '$lib/components/TextArea.svelte';
+	import type { ServiceResponse } from '$lib/types/ServiceResponse';
+	import PdfViewer from './PdfViewer.svelte';
+
+	export let ExecutionResponse: ServiceResponse;
+</script>
+
+<main>
+	<!-- Response Section -->
+	<div class="space-y-4">
+		<HeadingFormat as="h3" disabled={true}
+			>Response
+			<Tooltip value={ExecutionResponse.status.description}
+				>(Status code: {ExecutionResponse.status.code})</Tooltip
+			>
+		</HeadingFormat>
+
+		<HeadingFormat as="h4" disabled={true}>Raw body</HeadingFormat>
+		{#if ExecutionResponse.response.some((variable) => 'content' in variable)}
+			<PdfViewer
+				data={`data:application/pdf;base64,` +
+					ExecutionResponse.response.find((v) => 'content' in v)?.content[0]}
+			/>
+		{:else}
+			<List type="ul">
+				{#each ExecutionResponse.response as variableResponse, index (index)}
+					{#each Object.entries(variableResponse) as [key, value], innerIndex (innerIndex)}
+						<li><b>{key}:</b> {value.join(', ')}</li>
+					{/each}
+				{/each}
+			</List>
+			<TextArea
+				disabled={true}
+				text={ExecutionResponse.response ? JSON.stringify(ExecutionResponse.response) : ''}
+			/>
+		{/if}
+	</div>
+</main>
