1b1e74efc86aa4fee75a755568da9f2d18acfc03
DGutierrezLeal <a01568890@tec.mx>
2025-06-06 19:02:21 -0600

    chore: Gustavo said that he prefered this one (INT-07)

--- src/routes/endpoint/[id]/+page.svelte
+++ src/routes/endpoint/[id]/+page.svelte
@@ -25,6 +25,8 @@
 	let disabledBackwards: boolean = $state(false);
 	let selectedFilterIds = $state<number[]>([]);
 
+	let isExecuting = $state(false);
+
 	const emptyEndpoint: DetailedService = {
 		id: data.id,
 		name: '',
@@ -170,11 +172,13 @@
 
 	const handleSend = async () => {
 		try {
+			isExecuting = true;
 			await executeEndpoint();
 			console.log(requestService);
 			phaseIndex = 2;
 			disabledForward = true;
 			disabledBackwards = false;
+			isExecuting = false;
 		} catch (error) {
 			if (error) {
 				console.log(`There was an error with the request for the endpoint: ${data.id}`);
@@ -194,47 +198,52 @@
 </script>
 
 <main class="space-y-10 p-10">
-	{#if endpoint.name === ''}
-		<Spinner variant="dots"></Spinner>
+	{#if isExecuting}
+	<Spinner variant="dots"></Spinner>
 	{:else}
-		{#if phase[phaseIndex] === 'results'}
-			<DisplayResultSectionInvokeService {ExecutionResponse} />
-		{:else if phase[phaseIndex] == 'variables'}
-			<ParameterSectionInvokeService
-				{endpoint}
-				{variableTypes}
-				{variableTypeHeadingMap}
-				{findRequestKeyValue}
-			/>
-		{:else if phase[phaseIndex] == 'filters'}
-			<FilterSectionInvokeService
-				filters={endpoint.filters}
-				selected={selectedFilterIds}
-				on:filterChange={(e) => handleFilterChange(e.detail)}
-			/>
+		{#if endpoint.name === ''}
+			<Spinner variant="dots"></Spinner>
 		{:else}
-			<p>Nothing</p>
-		{/if}
+			{#if phase[phaseIndex] === 'results'}
+				<DisplayResultSectionInvokeService {ExecutionResponse} />
+			{:else if phase[phaseIndex] == 'variables'}
+				<ParameterSectionInvokeService
+					{endpoint}
+					{variableTypes}
+					{variableTypeHeadingMap}
+					{findRequestKeyValue}
+				/>
+			{:else if phase[phaseIndex] == 'filters'}
+				<FilterSectionInvokeService
+					filters={endpoint.filters}
+					selected={selectedFilterIds}
+					on:filterChange={(e) => handleFilterChange(e.detail)}
+				/>
+			{:else}
+				<p>Nothing</p>
+			{/if}
 
-		<div class="flex w-full items-center justify-between">
-			<div>
-				<Button
-					type="button"
-					size="md"
-					variant="secondary"
-					onClick={handlePhaseChangeBackward}
-					disabled={disabledBackwards}
-				>
-					Return
-				</Button>
+			<div class="flex w-full items-center justify-between">
+				<div>
+					<Button
+						type="button"
+						size="md"
+						variant="secondary"
+						onClick={handlePhaseChangeBackward}
+						disabled={disabledBackwards}
+					>
+						Return
+					</Button>
+				</div>
+				<div>
+					{#if phase[phaseIndex] === 'filters'}
+						<Button variant="primary" type="submit" size="md" onClick={handleSend}>Send</Button>
+					{:else if phase[phaseIndex] !== 'results'}
+						<Button onClick={handlePhaseChangeForward} disabled={disabledForward}>Next</Button>
+					{/if}
+				</div>
 			</div>
-			<div>
-				{#if phase[phaseIndex] === 'filters'}
-					<Button variant="primary" type="submit" size="md" onClick={handleSend}>Send</Button>
-				{:else if phase[phaseIndex] !== 'results'}
-					<Button onClick={handlePhaseChangeForward} disabled={disabledForward}>Next</Button>
-				{/if}
-			</div>
-		</div>
+		{/if}
 	{/if}
+	
 </main>
