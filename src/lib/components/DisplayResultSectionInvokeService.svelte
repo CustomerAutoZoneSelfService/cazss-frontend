ee7bd6bffa9e11c2c71c8d1f6ff7fef45464f525
LouArcTec <a01562967@tec.mx>
2025-06-08 11:35:17 -0600

    refactor: badge color and textArea display changes (FE-24)

--- src/lib/components/DisplayResultSectionInvokeService.svelte
+++ src/lib/components/DisplayResultSectionInvokeService.svelte
@@ -74,8 +74,8 @@
 				<TextArea
 					height={400}
 					disabled={true}
-					text={ExecutionResponse.response
-						? JSON.stringify(ExecutionResponse.response, null, 2)
+					text={ExecutionResponse.response && ExecutionResponse.response['-1']
+						? ExecutionResponse.response['-1'].join('\n')
 						: ''}
 				/>
 			{/if}
